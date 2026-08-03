eded3f8779a654a3c1dff654cf6d146dbdae50cc
Cosmonations <chuckansong@gmail.com>
2026-08-03 20:04:03 +0000

    Final Implementation of Student Grading System v3.2

---
+++ StudentGradingSystem v3.2.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream> 
+using namespace std;
+
+int main(){
+    string studentName;
+    int subjectNum;
+    double markSum = 0;
+    cout << "========================================" << endl;
+    cout << "    ACADEMIC TERM COMPILATION ENGINE"<< endl;
+    cout << "========================================" << endl;
+
+    cout << "Enter Student Full Name: ";
+    getline(cin, studentName);
+    cout << endl;
+
+    cout << "How many sujects did " << studentName << " take this term? ";
+    cin >> subjectNum;
+    cout << endl;
+
+    for (int i=1; i <= subjectNum; i++)
+    {
+        double currentMark = -1;
+        while (currentMark < 0 || currentMark > 100)
+        {
+            cout << " -> Enter raw percentage mark for Subject #" << i << " (0-100): ";
+            cin >> currentMark;
+            if (currentMark < 0 || currentMark > 100) {
+                cout << "[ERROR] Invalid mark configuration. Re-enter data." << endl;
+            }
+        }
+        markSum += currentMark;
+    }
+    cout << endl;
+
+    double termMeanAverage = markSum / subjectNum;
+    char finalLetterGrade;
+    if (termMeanAverage >= 80)
+        finalLetterGrade = 'A';
+    else if (termMeanAverage >= 70)
+        finalLetterGrade = 'B';
+    else if (termMeanAverage >= 60)
+        finalLetterGrade = 'C';
+    else if (termMeanAverage >= 50)
+        finalLetterGrade = 'D';
+    else
+        finalLetterGrade = 'F';
+
+    cout << "========================================" << endl;
+    cout << "    OFFICIAL PERFORMANCE REPORT"<< endl;
+    cout << "========================================" << endl;
+    cout << "Student Profile: "<< studentName << endl;
+    cout << "Total Classes: " << subjectNum << endl;
+    cout << "Terminal Mean: " << termMeanAverage << endl;
+    cout << "Final Evaluation: Grade[" << finalLetterGrade << "]" << endl;
+    
+    fstream recordFile;
+
+    recordFile.open("student_records.csv", ios::out); 
+    
+    if (recordFile.is_open()) {
+        recordFile << "FullName" << " SubjectNumber" << " TermGradeAverage" << " FinalTermGrade" << endl;
+        recordFile << studentName << " " << subjectNum << " " << termMeanAverage << " " << finalLetterGrade << endl;
+        recordFile.close();
+        cout << "Success! Student data successfully written to student_records.csv" << endl;
+    } else {
+        cout << "[ERROR] Could not open student_records.csv for writing." << endl;
+    }
+    
+    return 0;
+}
